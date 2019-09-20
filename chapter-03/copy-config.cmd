2a7d587a1928b1dee2d64e3feea6e55798573737
John M. Wargo <johnwargo@users.noreply.github.com>
2019-09-20 13:50:50 -0400

    updated Fri 09/20/2019

--- chapter-03/copy-config.cmd
+++ chapter-03/copy-config.cmd
@@ -4,10 +4,7 @@ ECHO Checking for compiled config.js
 IF EXIST "config.js" (
   ECHO Copying configuration file
   xcopy .\config.js ..\chapter-04\ /y
-  xcopy .\config.js ..\chapter-05\ /y
-  xcopy .\config.js ..\chapter-06\ /y
-  xcopy .\config.js ..\chapter-07\ /y
-  xcopy .\config.js ..\chapter-08\ /y
+  xcopy .\config.js ..\chapter-05\pwa-news\ /y 
 ) ELSE (
   ECHO File does not EXIST
   ECHO Please run the `tsc` command, and try again
--- chapter-03/copy-config.sh
+++ chapter-03/copy-config.sh
@@ -4,10 +4,7 @@ echo "Checking for compiled config.js"
 FILE=./config.js
 if test -f "$FILE"; then
   cp -v "$FILE" ../chapter-04
-  cp -v "$FILE" ../chapter-05
-  cp -v "$FILE" ../chapter-06
-  cp -v "$FILE" ../chapter-07  
-  cp -v "$FILE" ../chapter-08
+  cp -v "$FILE" ../chapter-05/pwa-news  
 else 
   echo $FILE does not exist
   echo Please run the tsc command, and try again
