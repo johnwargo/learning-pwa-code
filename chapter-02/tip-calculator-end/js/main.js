4adaef3fc7798e2726388906bc86d08accf71ebe
John M. Wargo <johnwargo@users.noreply.github.com>
2019-12-22 08:38:19 -0500

    updated Sun 12/22/2019

--- chapter-02/tip-calculator-end/js/main.js
+++ chapter-02/tip-calculator-end/js/main.js
@@ -13,7 +13,7 @@
     function updateTipAmounts() {
         // grab the meal cost from the page
         let mealCost = document.getElementById("mealCost").value;
-        // Populate the table with tip amounts
+        // populate the table with tip amounts
         document.getElementById('tip10').innerHTML = formatter.format(mealCost * 0.10);
         document.getElementById('tip15').innerHTML = formatter.format(mealCost * 0.15);
         document.getElementById('tip18').innerHTML = formatter.format(mealCost * 0.18);
@@ -23,24 +23,24 @@
 
     function doInstall() {
         console.log('doInstall');
-        // We've tapped the install button, so hide it
+        // we've tapped the install button, so hide it
         installButton.style.display = 'none';
         // execute the deferred installation prompt
         deferredPrompt.prompt();
-        // Wait for the response from the deferred prompt
-        deferredPrompt.userChoice.then((res) => {
+        // wait for the response from the deferred prompt
+        deferredPrompt.userChoice.then(res => {
             // did the user approve installation?
             if (res.outcome === 'accepted') {
                 console.log('doInstall: accepted');
             } else {
                 console.log('doInstall: declined');
             }
-            // Clear the deferred prompt object so we can only do this once
+            // clear the deferred prompt object so we can only do this once
             deferredPrompt = null;
         });
     }
 
-    // Register the event listener for the input field
+    // register the event listener for the input field
     document.getElementById('mealCost').oninput = updateTipAmounts;
 
     // did we launch as a PWA?
@@ -48,24 +48,24 @@
     // look for the source parameter, if it's `pwa` then it's installed
     if (urlParams.get('source') === 'pwa') {
         console.log('Launched as PWA');
-        // Add the PWA moniker to the title
+        // add the PWA moniker to the title
         let theTitle = document.getElementById('title');        
         theTitle.innerHTML = theTitle.innerHTML + ' (PWA)';
     }
 
-    // Get a handle to the install button
+    // get a handle to the install button
     let installButton = document.getElementById('installButton');
-    // Now set the click handler for the install button
+    // now set the click handler for the install button
     installButton.onclick = doInstall;
 
-    // Create an object we'll use to hold the reference to the PWA install
+    // create an object we'll use to hold the reference to the PWA install
     // event
     let deferredPrompt;
 
-    // Now add an event listener to respond to the event. Right before the browser
+    // now add an event listener to respond to the event. Right before the browser
     // installs the PWA, it fires the beforeinstallprompt event. Here, we'll manage
     // the installation ourselves
-    window.addEventListener('beforeinstallprompt', (event) => {
+    window.addEventListener('beforeinstallprompt', event => {
         console.log('Event: beforeinstallprompt')
         // don't allow the browser to do its install, we want to do it when the user
         // taps our install button
@@ -73,15 +73,13 @@
         // stash the event object so we can use it later (when the user taps the 
         // install button)
         deferredPrompt = event;
-        // Now unhide the Install button so the user can tap it!
+        // now unhide the Install button so the user can tap it!
         installButton.style.display = 'block';
     });
 
-    // Register an event listener for after the app installs
-    window.addEventListener('appinstalled', (event) => {
+    // register an event listener for after the app installs
+    window.addEventListener('appinstalled', event => {
         console.log('App Installed');
     });
 
-
 })();
-
